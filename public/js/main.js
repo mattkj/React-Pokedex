4165596dfc120258c3bf3b12ded7cb2c33335fbe
Matt Johnson <computermonkey@gmail.com>
2016-02-07 17:26:50 -0800

    start to implement filtering

--- public/js/main.js
+++ public/js/main.js
@@ -19473,14 +19473,25 @@ var Pokedex = React.createClass({
   },
   componentDidMount: function () {
     HTTP.get('/api/v1/pokedex/1/').then(function (data) {
+      console.log("data:", data);
       this.setState({ pokemonList: data });
     }.bind(this));
   },
+  filterPokemon(filterBy) {
+    var newList = this.state.pokemonList.pokemon.slice(0, 24).filter(function (pokemon) {
+      return pokemon.name <= filterBy;
+    });
+    console.log("filtered list:", newList);
+  },
   render: function () {
     if (this.state.pokemonList) {
+      console.log("original list:", this.state.pokemonList.pokemon.slice(0, 24));
+
       var displayPokemon = this.state.pokemonList.pokemon.slice(0, 24).map(function (pokemon) {
         return React.createElement(Pokemon, { key: pokemon.name, url: pokemon.resource_uri });
       });
+
+      this.filterPokemon("c");
     }
 
     return React.createElement(
